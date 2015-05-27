2044165d00c2b5ca503b089b3b8f31bebb82a341
Tim Brandin <tim.brandin@gmail.com>
2015-05-27 11:33:22 -0700

    Bump.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'studiointeract:slack-invite',
-  version: '1.0.4',
+  version: '1.0.5',
   // Brief, one-line summary of the package.
   summary: 'Template and form to add a Slack invite box on your app.',
   // URL to the Git repository containing the source code for this package.
